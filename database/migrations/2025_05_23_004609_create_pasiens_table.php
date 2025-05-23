c2865548ff8afca8e75d0a2a8fd3f3c4c5a6127f
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-23 08:32:00 +0700

    feat: create Pasien model, controller, and migration for pasiens table

---
+++ database/migrations/2025_05_23_004609_create_pasiens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('alamat');
+            $table->string('no_ktp')->unique();
+            $table->string('no_hp', 50)->unique();
+            $table->string('no_rm', 25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pasiens');
+    }
+};
