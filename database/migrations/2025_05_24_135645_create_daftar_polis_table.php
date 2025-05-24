a823b4773bbf59a26d163c7d2206a49fd8c63a2d
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-24 21:52:24 +0700

    feat: create DaftarPoli model, controller, and migration for daftar_polis table

---
+++ database/migrations/2025_05_24_135645_create_daftar_polis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daftar_polis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_pasien')->constrained('pasiens')->onDelete('cascade');
+            $table->foreignId('id_jadwal')->constrained('jadwal_periksas')->onDelete('cascade');
+            $table->longText('keluhan');
+            $table->unsignedInteger('no_antrian')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daftar_polis');
+    }
+};
