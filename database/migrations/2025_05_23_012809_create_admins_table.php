22a4ea15e566f2c7a83c976e49705e0f8d7f727a
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-25 22:10:59 +0700

    refactor: remove 'name' and 'no_hp' fields from admins table migration

--- database/migrations/2025_05_23_012809_create_admins_table.php
+++ database/migrations/2025_05_23_012809_create_admins_table.php
@@ -14,9 +14,7 @@ return new class extends Migration
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('name');
             $table->string('alamat');
-            $table->string('no_hp');
             $table->timestamps();
         });
     }
