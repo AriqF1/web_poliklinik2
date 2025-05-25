5b6f373c3c735410d0bc2a6fa9c748248b1de0f6
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-25 22:18:17 +0700

    refactor: define fillable attributes and user relationship in Pasien model

--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pasien extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'alamat',
+        'no_ktp',
+        'no_hp',
+        'no_rm',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
