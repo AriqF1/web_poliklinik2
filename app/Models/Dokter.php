60007588f61a585375d43186c7d7341f5dfe605e
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-25 22:18:07 +0700

    refactor: add fillable attributes and user relationship in Dokter model

--- app/Models/Dokter.php
+++ app/Models/Dokter.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Dokter extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'alamat',
+        'no_hp',
+        'poli_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
