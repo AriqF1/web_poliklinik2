b041f5c842f341ea202a7fae1ec277fd6cc61c4e
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-25 22:45:58 +0700

    refactor: add relationship methods for poli and jadwalPeriksa in Dokter model

--- app/Models/Dokter.php
+++ app/Models/Dokter.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Dokter extends Model
 {
@@ -17,5 +18,15 @@ class Dokter extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function poli(): BelongsTo
+    {
+        return $this->belongsTo(Poli::class, 'poli_id');
+    }
+
+    public function jadwalPeriksa(): HasMany
+    {
+        return $this->hasMany(JadwalPeriksa::class, 'id_dokter');
     }
 }
