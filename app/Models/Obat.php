3e1998d1d788c28f025cb598deb1fcf5c22fc068
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-25 22:46:19 +0700

    refactor: define fillable attributes and relationship method in Obat model

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Obat extends Model
 {
-    //
+    protected $fillable = [
+        'nama_obat',
+        'kemasan',
+        'harga',
+    ];
+
+    public function detailPeriksa(): HasOne
+    {
+        return $this->hasOne(DetailPeriksa::class, 'id_obat');
+    }
 }
