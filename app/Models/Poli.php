190cca0d9a480469cbea5afdd276c5e4ab4fc3b9
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-25 22:46:48 +0700

    refactor: define fillable attributes and relationship method in Poli model

--- app/Models/Poli.php
+++ app/Models/Poli.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Poli extends Model
 {
-    //
+    protected $fillable = [
+        'nama_poli',
+        'keterangan',
+    ];
+
+    public function dokter(): HasOne
+    {
+        return $this->hasOne(Dokter::class, 'poli_id');
+    }
 }
