f7976cede8c8f971b5588abe5cceb6245796069f
Ariq Farhan <ariqalthaf445@gmail.com>
2025-05-25 22:45:37 +0700

    refactor: enhance DaftarPoli model with fillable attributes and relationship methods

--- app/Models/DaftarPoli.php
+++ app/Models/DaftarPoli.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class DaftarPoli extends Model
 {
-    //
+    protected $fillable = [
+        'id_pasien',
+        'id_jadwal',
+        'keluhan',
+        'no_antrian'
+    ];
+
+    public function pasien(): BelongsTo
+    {
+        return $this->belongsTo(Pasien::class, 'id_pasien');
+    }
+
+    public function jadwalPeriksa(): BelongsTo
+    {
+        return $this->belongsTo(JadwalPeriksa::class, 'id_jadwal');
+    }
+
+    public function periksa(): HasOne
+    {
+        return $this->hasOne(Periksa::class, 'id_daftar_poli');
+    }
 }
