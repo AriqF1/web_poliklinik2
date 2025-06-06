0d7a9fbeed8164d9289598e24612c66d4ce53925
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-06 14:21:51 +0700

    feat: set title for CreateObat page to 'Tambah Obat'

--- app/Filament/Resources/ObatResource/Pages/CreateObat.php
+++ app/Filament/Resources/ObatResource/Pages/CreateObat.php
@@ -4,9 +4,11 @@ namespace App\Filament\Resources\ObatResource\Pages;
 
 use App\Filament\Resources\ObatResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateObat extends CreateRecord
 {
     protected static string $resource = ObatResource::class;
+    protected static ?string $title = 'Tambah Obat';
 }
