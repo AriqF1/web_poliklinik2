21babebffbb9dee244f86c60f1a4e003e9d027d3
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-06 14:21:26 +0700

    feat: set title for ListObats page to 'Daftar Obat'

--- app/Filament/Resources/ObatResource/Pages/ListObats.php
+++ app/Filament/Resources/ObatResource/Pages/ListObats.php
@@ -9,6 +9,7 @@ use Filament\Resources\Pages\ListRecords;
 class ListObats extends ListRecords
 {
     protected static string $resource = ObatResource::class;
+    protected static ?string $title = 'Daftar Obat';
 
     protected function getHeaderActions(): array
     {
