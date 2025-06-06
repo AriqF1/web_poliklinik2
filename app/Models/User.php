45575d0bcd172c270138569dc4ac8ab516e020cc
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-06 21:13:55 +0700

    refactor: remove unused admin relationship from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -45,11 +46,6 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function admin(): HasOne
-    {
-        return $this->hasOne(Admin::class);
     }
 
     public function pasien(): HasOne
