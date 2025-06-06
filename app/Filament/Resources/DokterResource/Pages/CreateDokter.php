632f3586f22d82a1650eb72be08a7acd9cfad9f9
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-06 19:26:48 +0700

    refactor: clean up comments in CreateDokter page's user creation logic

--- app/Filament/Resources/DokterResource/Pages/CreateDokter.php
+++ app/Filament/Resources/DokterResource/Pages/CreateDokter.php
@@ -12,18 +12,16 @@ class CreateDokter extends CreateRecord
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        // Ambil data user
+
         $userData = $data['user'] ?? [];
         unset($data['user']);
 
-        // Buat user dulu
         $user = \App\Models\User::create([
             ...$userData,
             'role' => 'dokter',
             'password' => bcrypt($userData['password']),
         ]);
 
-        // Masukkan user_id ke data dokter
         $data['user_id'] = $user->id;
 
         return $data;
