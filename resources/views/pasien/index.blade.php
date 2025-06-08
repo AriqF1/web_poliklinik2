562949f5fb6da0c47ac3d062a79eb39bf56fc6a2
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-08 11:24:52 +0700

    feat: implement dashboard layout for Pasien with card components

--- resources/views/pasien/index.blade.php
+++ resources/views/pasien/index.blade.php
@@ -1,15 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
+@php
+    $showTables = false;
+    $showFullscreen = false;
+@endphp
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@extends('layout.index')
 
-<body>
-    <h1>Halaman Pasien</h1>
-</body>
+@section('title', 'Dashboard | Pasien')
+@section('dashboard', 'Pasien')
+@section('header')
+    <h1>Dashboard Pasien</h1>
+    <div class="user-info">
+        <div class="user-avatar">
+            <i class="fas fa-user-md"></i>
+        </div>
+        {{-- <div class="user-name">{{ $pasien->nama }}</div> --}}
+    </div>
+@endsection
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Total Dokter</div>
+            <div class="card-icon">
+                <i class="fas fa-users"></i>
+            </div>
+        </div>
+        {{-- <div class="card-value">{{ $jumlahDokter }}</div> --}}
+        <div class="card-label">Dokter Tersedia</div>
+    </div>
 
-</html>
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Janji Hari Ini</div>
+            <div class="card-icon">
+                <i class="fas fa-calendar-check"></i>
+            </div>
+        </div>
+        {{-- <div class="card-value">{{ $janjiTemu }}</div> --}}
+        <div class="card-label">Janji temu</div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Selesai</div>
+            <div class="card-icon">
+                <i class="fas fa-check-circle"></i>
+            </div>
+        </div>
+        {{-- <div class="card-value">{{ $jumlahPemeriksaan }}</div> --}}
+        <div class="card-label">Total Periksamu</div>
+    </div>
+@endsection
