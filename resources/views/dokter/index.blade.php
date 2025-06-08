a4fab423b4e315029bde96dae24fb126b9ff4e4c
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-08 11:24:29 +0700

    feat: restructure Dokter dashboard layout with Blade templating

--- resources/views/dokter/index.blade.php
+++ resources/views/dokter/index.blade.php
@@ -1,15 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
+@php
+    $showTables = false;
+    $showFullscreen = false;
+@endphp
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
 
-<body>
-    <h1>Halaman Dokter</h1>
-</body>
+@extends('layout.index')
 
-</html>
+@section('title', 'Dashboard Dokter')
+@section('dashboard', 'Dokter')
+@section('header')
+    <h1>Dashboard Dokter</h1>
+    <div class="user-info">
+        <div class="user-avatar">
+            <i class="fas fa-user-md"></i>
+        </div>
+        {{-- <div class="user-name">{{ $dokter->nama }}</div> --}}
+    </div>
+
+@endsection
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Pasien Anda</div>
+            <div class="card-icon">
+                <i class="fas fa-users"></i>
+            </div>
+        </div>
+        {{-- <div class="card-value">{{ $jumlahPasien }}</div> --}}
+        <div class="card-label">Jumlah Antrian Pasien Anda</div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Janji Hari Ini</div>
+            <div class="card-icon">
+                <i class="fas fa-calendar-check"></i>
+            </div>
+        </div>
+        {{-- <div class="card-value">{{ $janjiTemu }}</div> --}}
+        <div class="card-label">Janji temu </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Selesai</div>
+            <div class="card-icon">
+                <i class="fas fa-check-circle"></i>
+            </div>
+        </div>
+        {{-- <div class="card-value">{{ $jumlahPemeriksaan }}</div> --}}
+        <div class="card-label">Total Pemeriksaan Selesai</div>
+    </div>
+@endsection
+@section('table')
+
+
+@endsection
