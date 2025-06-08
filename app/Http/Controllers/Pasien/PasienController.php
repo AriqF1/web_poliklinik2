1f25e63a28f2ebc891d589f5bcfa6e7a41d777d4
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-08 21:08:25 +0700

    feat: update index method in PasienController to retrieve user and doctor count for view

--- app/Http/Controllers/Pasien/PasienController.php
+++ app/Http/Controllers/Pasien/PasienController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Pasien;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pasien;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class PasienController extends Controller
@@ -13,7 +15,9 @@ class PasienController extends Controller
      */
     public function index()
     {
-        return view('pasien.index');
+        $pasien = User::where('id', Auth::id())->first();
+        $totalDokter = User::where('role', 'dokter')->count();
+        return view('pasien.index', compact('pasien', 'totalDokter'));
     }
 
     /**
