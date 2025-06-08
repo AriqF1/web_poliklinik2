1b8e97483a01742de912b1c0634d4ee4fb32be03
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-08 11:25:04 +0700

    fix: remove debug statement from login method in LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,8 +31,6 @@ class LoginController extends Controller
             // Ambil user yang sedang login
             $user = Auth::User();
 
-            dd($user);
-
             // Cek apakah user memiliki role yang sesuai
             if ($user->role) {
                 // Redirect sesuai dengan role user
