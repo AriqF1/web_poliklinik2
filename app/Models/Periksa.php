50dc2860439569ab94e4f6d0b78de5bd60956e2d
Ariq Farhan <ariqalthaf445@gmail.com>
2025-06-13 22:03:19 +0700

    feat: implement validation and storage for patient examination in PeriksaController

--- app/Models/Periksa.php
+++ app/Models/Periksa.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Periksa extends Model
 {
@@ -20,8 +20,8 @@ class Periksa extends Model
         return $this->belongsTo(DaftarPoli::class, 'id_daftar_poli');
     }
 
-    public function detailPeriksa(): HasOne
+    public function detailPeriksa(): HasMany
     {
-        return $this->hasOne(DetailPeriksa::class, 'id_periksa');
+        return $this->hasMany(DetailPeriksa::class, 'id_periksa');
     }
 }
